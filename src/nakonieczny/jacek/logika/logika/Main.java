4fb56410633c9fc4d13f1a14f7f440f6944e3a76
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2012-12-17 09:13:02 +0100

    Ćwiczenia 11.12.12

---
+++ src/nakonieczny/jacek/logika/logika/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nakonieczny.jacek.logika.logika;
+
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import nakonieczny.jacek.logika.dane.Punkt;
+import nakonieczny.jacek.logika.widok.MainFrame;
+
+/**
+ *
+ * @author Jacek
+ */
+public class Main {
+
+    private static MainFrame frame;
+
+    public static void main(String[] args) {
+        frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+        //<editor-fold defaultstate="collapsed" desc="TEST">
+        //</editor-fold>
+    }
+}
