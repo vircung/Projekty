1f5286257f81709d47394ed72ad778d318376fb8
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2013-03-01 08:56:30 +0100

    Kolejne rysowanie kształtów.

---
+++ JAVA/src/nakonieczny/jacek/rysowanie/logic/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nakonieczny.jacek.rysowanie.logic;
+
+/**
+ *
+ * @author Jacek
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+    }
+}
